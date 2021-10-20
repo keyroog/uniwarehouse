4c3ca4d58acf7a39f29dafa43cd354c7773acc9f
plantrug <92570564+plantrug@users.noreply.github.com>
2021-10-20 20:29:30 +0200

    piccoli modifiche

--- src/control/Annuncio_Servlet.java
+++ src/control/Annuncio_Servlet.java
@@ -1,11 +1,9 @@
 package control;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
